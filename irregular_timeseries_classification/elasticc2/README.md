085bbe3d48edb6384b75da2467327251916b92f1
chromeilion <uki@cro.moe>
2025-03-19 18:17:12 +0100

    work on elasticc experiment

---
+++ irregular_timeseries_classification/elasticc2/README.md
@@ -0,0 +1,8 @@
+# ELAsTiCC 2 Hierarchical Classification
+
+Competing with [this](https://arxiv.org/abs/2501.01496) paper. Also competing 
+with [this](https://arxiv.org/abs/2502.18558) paper. A bunch of data processing code is taken from [this](https://github.com/uiucsn/ELAsTiCC-Classification?tab=readme-ov-file) 
+repo. ELasTICC was originally announced [here](https://baas.aas.org/pub/2023n2i358p04/release/1).
+and [here](https://baas.aas.org/pub/2023n2i117p01/release/1).
+
+
---
+++ irregular_timeseries_classification/elasticc2/elasticc2/dataset.py
@@ -0,0 +1,28 @@
+"""
+A simple example dataset.
+"""
+import torch
+from torch.utils.data.dataset import Dataset
+
+
+class ExampleDataset(Dataset):
+    """
+    Very simple dataset where the label is just the mean of the input.
+    """
+    def __init__(self, n_samples: int = 1000):
+        self.labels = torch.zeros(n_samples).uniform_(0, 100)
+
+    def __len__(self):
+        return self.labels.shape[0]
+
+    def __getitem__(self, index):
+        # A random value between 0 and 100
+        mean = self.labels[index]
+        # The expected shape is TCHW, so we add in 3 extra dimensions
+        value = torch.zeros(100).normal_(mean=mean.item())[:, None, None, None]
+        sample = {
+            "values": value,
+            "positions": torch.zeros((3, 50)),
+            "label": mean
+        }
+        return sample
