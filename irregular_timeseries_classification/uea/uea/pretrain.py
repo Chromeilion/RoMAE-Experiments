389e6fe745f7f12a13114a8c32bbdda093ae3c9e
Chromeilion <uki@cro.moe>
2025-05-10 23:56:54 +0200

    fine-tune a subset of the irregular datasets

--- irregular_timeseries_classification/uea/uea/pretrain.py
+++ irregular_timeseries_classification/uea/uea/pretrain.py
@@ -3,11 +3,15 @@ from pathlib import Path
 
 import torch
 from torch.utils.data import DataLoader
+import torch.nn as nn
 import tqdm
 from roma.trainer import TrainerConfig, Trainer
-from roma.model import RoMAForPreTraining, RoMAForPreTrainingConfig, EncoderConfig, RoMAForClassification
+from roma.model import RoMAForPreTraining, RoMAForPreTrainingConfig, \
+    EncoderConfig, RoMAForClassification, RoMAForClassificationConfig
 from roma.utils import get_encoder_size
 from sklearn.metrics import classification_report
+import random
+import numpy as np
 
 from uea.config import UEAConfig
 from uea.dataset import UEADataset
@@ -18,76 +22,195 @@ def pretrain():
     Pre-training script which will train RoMAForPreTraining on the data.
     """
     config = UEAConfig()
-    datasets = os.listdir(config.dataset_dir)
-    for dataset_name in ["LSST"]:#datasets:
-        pretrain_on_ds(config.dataset_dir, dataset_name)
+    runs = [
+        {
+            "dataset": "CharacterTrajectories",
+            "model": "RoMA-tiny",
+            "ft_lr": 8e-3,
+            "ft_epochs": 100,
+            "ft_batch_size": 16,
+            "ft_eval_every": 250,
+            "ft_save_every": 250,
+            "do_pretrain": True,
+            "ft_optimizer": "sgd",
+            "ft_gradient_clip": 1,
+            "ft_optimizer_args": {"weight_decay": 0., "momentum": 0.9},
+            "ft_label_smoothing": 0.1,
+            "ft_drop_path": 0.,
+            "ft_hidden_drop_rate": 0.,
+            "ft_attn_drop_rate": 0.
+        },
+        {
+            "dataset": "BasicMotions",
+            "model": "RoMA-tiny",
+            "ft_lr": 1e-2,
+            "ft_epochs": 50,
+            "ft_batch_size": 8,
+            "ft_eval_every": 10,
+            "ft_save_every": 10,
+            "do_pretrain": True,
+            "ft_optimizer": "sgd",
+            "ft_gradient_clip": 1,
+            "ft_optimizer_args": {"weight_decay": 0., "momentum": 0.9},
+            "ft_label_smoothing": 0.,
+            "ft_drop_path": 0.,
+            "ft_hidden_drop_rate": 0.,
+            "ft_attn_drop_rate": 0.
+        },
+        {
+            "dataset": "LSST",
+            "model": "RoMA-tiny",
+            "ft_lr": 3e-2,
+            "ft_epochs": 15,
+            "ft_batch_size": 16,
+            "ft_eval_every": 100,
+            "ft_save_every": 100,
+            "ft_gradient_clip": 10,
+            "do_pretrain": True,
+            "ft_optimizer": "sgd",
+            "ft_optimizer_args": {"weight_decay": 0., "momentum": 0.9},
+            "ft_label_smoothing": 0.1,
+            "ft_drop_path": 0.2,
+            "ft_hidden_drop_rate": 0.2,
+            "ft_attn_drop_rate": 0.2
+            "ft_warmup_percentage": 0.1
+        },
+        {
+            "dataset": "Epilepsy",
+            "model": "RoMA-tiny",
+            "ft_lr": 2e-3,
+            "ft_epochs": 150,
+            "ft_batch_size": 16,
+            "ft_eval_every": 100,
+            "ft_save_every": 100,
+            "ft_gradient_clip": 1,
+            "do_pretrain": True,
+            "ft_optimizer": "sgd",
+            "ft_optimizer_args": {"weight_decay": 0., "momentum": 0.9},
+            "ft_label_smoothing": 0.2,
+            "ft_drop_path": 0.2,
+            "ft_hidden_drop_rate": 0.2,
+            "ft_attn_drop_rate": 0.2
+            "ft_warmup_percentage": 0.1
+        },
+        {
+            "dataset": "Heartbeat",
+            "model": "RoMA-tiny",
+            "ft_lr": 2e-2,
+            "ft_epochs": 30,
+            "ft_batch_size": 32,
+            "ft_eval_every": 50,
+            "ft_save_every": 50,
+            "ft_gradient_clip": 2,
+            "do_pretrain": True,
+            "ft_optimizer": "sgd",
+            "ft_optimizer_args": {"weight_decay": 0., "momentum": 0.9},
+            "ft_label_smoothing": 0.,
+            "ft_drop_path": 0.,
+            "ft_hidden_drop_rate": 0.,
+            "ft_attn_drop_rate": 0.,
+            "ft_warmup_percentage": 0.1
+        }
+    ]
+    for run in runs:
+        for i in [27, 42, 1024]:
+            dataset_savedir = Path(f"./{run['dataset']}_run_{str(i)}/")
+            dataset_savedir.mkdir(exist_ok=True)
+            res = pretrain_on_ds(config.dataset_dir, run, dataset_savedir, seed=i+42)
+            with open(dataset_savedir/"results.txt", "w") as f:
+                f.write(res)
+            print(f"Finished {run['dataset']} run {i}")
 
 
-def pretrain_on_ds(data_root, dataset_name: str, mask_percentage: float = 0.3):
-    top_savedir = Path(f"./all_results")
+def pretrain_on_ds(dataset_dir, run, dataset_savedir: Path = Path("."), mask_percentage: float = 0.3, seed=42):
+    torch.manual_seed(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    train_ds = UEADataset(prefix=dataset_dir, name=run["dataset"],
+                          mask_ratio=0.3,
+                          dataset="train",
+                          pretrain_mask_ratio=0.75)
+    test_ds = UEADataset(prefix=dataset_dir, name=run["dataset"],
+                         mask_ratio=0.3,
+                         dataset="test",
+                         pretrain_mask_ratio=0.75)
+    top_savedir = dataset_savedir
     top_savedir.mkdir(exist_ok=True)
-    savedir = Path(top_savedir/f"{dataset_name}_savedir")
+    savedir = Path(top_savedir/f"{run['dataset']}_savedir")
     savedir.mkdir(exist_ok=True)
-    train_ds = UEADataset(prefix=data_root, name=dataset_name,
-                    mask_ratio=mask_percentage,
-                    dataset="train",
-                    pretrain_mask_ratio=0.75)
-    test_ds = UEADataset(prefix=data_root, name=dataset_name,
-                    mask_ratio=mask_percentage,
-                    dataset="test",
-                    pretrain_mask_ratio=0.75)
-    encoder_args = get_encoder_size("RoMA-tiny")
+    encoder_args = get_encoder_size(run["model"])
     encoder_config = EncoderConfig(**encoder_args)
-    batch_size = 64
-    epochs = 800
-    trainer_config = TrainerConfig(
-        base_lr=3e-4,
-        epochs=epochs,
-        eval_every=500,
-        save_every=500,
-        batch_size=batch_size,
-        optimizer="adamw",
-        project_name=dataset_name,
-        optimizer_args={"betas": (0.9, 0.95), "weight_decay": 0.05},
-        checkpoint_dir=str(savedir/"pretrain"),
-        run_name=f"{dataset_name}_pretrain",
-        warmup_steps=int((len(train_ds)*epochs)//batch_size * 0.1),
-    )
-    model_config = RoMAForPreTrainingConfig(
-        encoder_config=encoder_config,
-        n_pos_dims=1,
-        n_channels=train_ds[0]["values"].shape[1],
-        tubelet_size=(1, 1, 1)
-    )
-    model = RoMAForPreTraining(model_config)
-    trainer = Trainer(trainer_config)
-    latest_checkpoint = get_latest_checkpoint(savedir/"pretrain")
-    trainer.train(
-        train_dataset=train_ds,
-        test_dataset=test_ds,
-        model=model,
-        checkpoint=latest_checkpoint
-    )
-    model = RoMAForClassification.from_pretrained(
-        latest_checkpoint,
-        dim_output=14
-    )
+    if run["do_pretrain"]:
+        batch_size = 64
+        epochs = 400
+        trainer_config = TrainerConfig(
+            random_seed=seed,
+            base_lr=3e-4,
+            epochs=epochs,
+            eval_every=500,
+            save_every=500,
+            batch_size=batch_size,
+            optimizer="adamw",
+            project_name=run["dataset"],
+            optimizer_args={"betas": (0.9, 0.95), "weight_decay": 0.05},
+            checkpoint_dir=str(savedir/"pretrain"),
+            run_name=f"{run['dataset']}_pretrain",
+            warmup_steps=int((len(train_ds)*epochs)//batch_size * 0.1),
+        )
+        model_config = RoMAForPreTrainingConfig(
+            encoder_config=encoder_config,
+            n_pos_dims=1,
+            n_channels=train_ds[0]["values"].shape[1],
+            tubelet_size=(1, 1, 1)
+        )
+        model = RoMAForPreTraining(model_config)
+        trainer = Trainer(trainer_config)
+        latest_checkpoint = get_latest_checkpoint(savedir/"pretrain")
+        trainer.train(
+            train_dataset=train_ds,
+            test_dataset=test_ds,
+            model=model,
+            checkpoint=latest_checkpoint
+        )
+        latest_checkpoint = get_latest_checkpoint(savedir/"pretrain")
+        encoder_config.drop_path_rate = run["ft_drop_path"]
+        encoder_config.hidden_drop_rate = run["ft_hidden_drop_rate"]
+        encoder_config.attn_drop_rate = run["ft_attn_drop_rate"]
+        model = RoMAForClassification.from_pretrained(
+            latest_checkpoint,
+            dim_output=train_ds.inner_ds.class_names.shape[0],
+            encoder_config=encoder_config
+        )
+    else:
+        encoder_config.drop_path_rate = run["ft_drop_path"]
+        encoder_config.hidden_drop_rate = run["ft_hidden_drop_rate"]
+        encoder_config.attn_drop_rate = run["ft_attn_drop_rate"]
+        config = RoMAForClassificationConfig(
+            encoder_config=encoder_config,
+            n_pos_dims=1,
+            n_channels=train_ds[0]["values"].shape[1],
+            tubelet_size=(1, 1, 1),
+            dim_output=train_ds.inner_ds.class_names.shape[0]
+        )
+        model = RoMAForClassification(config)
+    model.set_loss_fn(nn.CrossEntropyLoss(label_smoothing=run["ft_label_smoothing"]))
     latest_checkpoint = get_latest_checkpoint(savedir/"finetune")
-    epochs = 15
+    epochs = 20
     batch_size=16
     trainer_config = TrainerConfig(
-        base_lr=3e-3,
-        epochs=epochs,
-        gradient_clip=10,
-        eval_every=200,
-        save_every=200,
-        batch_size=batch_size,
-        optimizer="sgd",
-        project_name=dataset_name,
-        optimizer_args={"weight_decay": 0., "momentum": 0.9},
+        random_seed=seed,
+        base_lr=run["ft_lr"],
+        epochs=run["ft_epochs"],
+        gradient_clip=run["ft_gradient_clip"],
+        eval_every=run["ft_eval_every"],
+        save_every=run["ft_save_every"],
+        batch_size=run["ft_batch_size"],
+        optimizer=run["ft_optimizer"],
+        project_name=run["dataset"],
+        optimizer_args=run["ft_optimizer_args"],
         checkpoint_dir=str(savedir/"finetune"),
-        run_name=f"{dataset_name}_pretrain",
-        warmup_steps=int((len(train_ds)*epochs)//batch_size * 0.1),
+        run_name=f"{run['dataset']}_finetune",
+        warmup_steps=int((len(train_ds)*run["ft_epochs"])//batch_size * run["ft_warmup_percentage"]),
     )
     trainer = Trainer(trainer_config)
     trainer.train(
@@ -112,10 +235,9 @@ def pretrain_on_ds(data_root, dataset_name: str, mask_percentage: float = 0.3):
                                  dim=1)
             all_labels.extend(list(batch["label"].cpu().numpy()))
             all_preds.extend(list(preds.cpu().numpy()))
-    print(classification_report(
-        all_labels,
-        all_preds,
-    ))
+    cls_rep = classification_report(all_labels, all_preds, digits=4)
+    print(cls_rep)
+    return cls_rep
 
 
 def get_latest_checkpoint(savedir):
